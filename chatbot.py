49bc1311f6a866f84b3eceb0b2594505c3f6c0b9
Ali Essam <aliduku@gmail.com>
2023-08-20 13:26:07 +0300

    last

--- chatbot.py
+++ chatbot.py
@@ -1,101 +1,96 @@
-import random
 import streamlit as st
-import string
-import re, string, unicodedata
-import wikipedia as wk
-from collections import defaultdict
-from nltk.corpus import wordnet as wn
-from nltk.stem.wordnet import WordNetLemmatizer
+import re
 import nltk
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
+from nltk.corpus import stopwords
+from nltk.stem import wordnet
+from nltk import pos_tag, word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import pairwise_distances
+import pandas as pd
 
+# Download NLTK resources
 nltk.download('punkt')
 nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
 
-# Load or preprocess Wikipedia data
-data = open('wiki_train_raw.txt', 'r', errors='ignore')
-raw = data.read()
-raw = raw.lower()
-sent_tokens = nltk.sent_tokenize(raw)
+# Load dataset
+conversation_df = pd.read_excel("dialog_talk_agent.xlsx")
+conversation_df.ffill(axis=0, inplace=True)
 
-def Normalize(text):
-    remove_punct_dict = {ord(punct): None for punct in string.punctuation}
-    word_token = nltk.word_tokenize(text.lower().translate(remove_punct_dict))
-    new_words = []
-    for word in word_token:
-        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-        new_words.append(new_word)
-    rmv = []
-    for w in new_words:
-        text = re.sub("&lt;/?.*?&gt;", "&lt;&gt;", w)
-        rmv.append(text)
-    tag_map = defaultdict(lambda: wn.NOUN)
-    tag_map['J'] = wn.ADJ
-    tag_map['V'] = wn.VERB
-    tag_map['R'] = wn.ADV
-    lmtzr = WordNetLemmatizer()
-    lemma_list = []
-    rmv = [i for i in rmv if i]
-    for token, tag in nltk.pos_tag(rmv):
-        lemma = lmtzr.lemmatize(token, tag_map[tag[0]])
-        lemma_list.append(lemma)
-    return lemma_list
+# Normalization
+def normalize_text(text):
+    text = str(text).lower()
+    text = re.sub(r'[^a-z0-9]', " ", text)
+    tokens = word_tokenize(text)
+    lemmatizer = wordnet.WordNetLemmatizer()
+    
+    tagged_tokens = pos_tag(tokens, tagset=None)
+    token_lemmas = []
+    
+    for (token, pos_token) in tagged_tokens:
+        if pos_token.startswith("V"):  # verb
+            pos_val = "v"
+        elif pos_token.startswith("J"):  # adjective
+            pos_val = "a"
+        elif pos_token.startswith("R"):  # adverb
+            pos_val = "r"
+        else:
+            pos_val = 'n'  # noun
+        token_lemmas.append(lemmatizer.lemmatize(token, pos_val))
+        
+    return " ".join(token_lemmas)
 
-welcome_input = ("hello", "hi", "greetings", "sup", "what's up","hey",)
-welcome_response = ["hi", "hey", "hi there", "hello", "I am glad! You are talking to me"]
-def welcome(user_response):
-    for word in user_response.split():
-        if word.lower() in welcome_input:
-            return random.choice(welcome_response)
+conversation_df["normalized_text"] = conversation_df["Context"].apply(normalize_text)
 
-def generateResponse(user_response):
-    robo_response = ''
-    sent_tokens.append(user_response)
-    TfidfVec = TfidfVectorizer(tokenizer=Normalize, stop_words='english')
-    tfidf = TfidfVec.fit_transform(sent_tokens)
-    vals = linear_kernel(tfidf[-1], tfidf)
-    idx = vals.argsort()[0][-2]
-    flat = vals.flatten()
-    flat.sort()
-    req_tfidf = flat[-2]
-    return (
-        wikipedia_data(user_response)
-        if (req_tfidf == 0) or "tell me about" in user_response
-        else robo_response + sent_tokens[idx]
-    )
+# Remove stopwords
+def remove_stopwords(text):
+    stop = stopwords.words("english")
+    text = [word for word in text.split() if word not in stop]
+    return " ".join(text)
 
-# Wikipedia search
-def wikipedia_data(input):
-    reg_ex = re.search('tell me about (.*)', input)
-    try:
-        if reg_ex:
-            topic = reg_ex[1]
-            return wk.summary(topic, sentences=3)
-    except Exception as e:
-        return "No content has been found"
+# Bag of Words
+bow_vectorizer = CountVectorizer()
+bow_matrix = bow_vectorizer.fit_transform(conversation_df["normalized_text"]).toarray()
+bow_features = bow_vectorizer.get_feature_names_out()
+bow_df = pd.DataFrame(bow_matrix, columns=bow_features)
+
+# TF-IDF
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform(conversation_df["normalized_text"]).toarray()
+tfidf_df = pd.DataFrame(tfidf_matrix, columns=tfidf_vectorizer.get_feature_names_out())
+
+# Cosine Similarity using BOW
+def chat_bow(user_input):
+    normalized_input = normalize_text(user_input)
+    without_stopwords = remove_stopwords(normalized_input)
+    bow_input = bow_vectorizer.transform([without_stopwords]).toarray()
+    cosine_similarity = 1 - pairwise_distances(bow_df, bow_input, metric="cosine")
+    index_of_most_similar = cosine_similarity.argmax()
+    return conversation_df["Text Response"].loc[index_of_most_similar]
+
+# Cosine Similarity using TF-IDF
+def chat_tfidf(user_input):
+    normalized_input = normalize_text(user_input)
+    tfidf_input = tfidf_vectorizer.transform([normalized_input]).toarray()
+    cosine_similarity = 1 - pairwise_distances(tfidf_df, tfidf_input, metric="cosine")
+    index_of_most_similar = cosine_similarity.argmax()
+    return conversation_df["Text Response"].loc[index_of_most_similar]
 
 # Streamlit app
 def main():
-    st.title("WikiChat - Your Wikipedia Chatbot")
+    st.title("Chatbot")
 
-    st.write("My name is Chatterbot. Let's have a conversation! If you want to exit, just type 'Bye'.")
+    st.write("Welcome to the Chatbot! Type your message below.")
 
     user_input = st.text_area("You:", "")
-    submit_button = st.button("Submit")  # Add a "Submit" button
+    submit_button = st.button("Submit")
 
-    if submit_button and user_input:  # Wait for button click and non-empty input
-        user_input = user_input.lower()
-        if user_input in ['bye', 'shutdown', 'exit', 'quit']:
-            st.write("Chatterbot: Bye!!!")
-
-        elif(welcome(user_input) != None):
-            st.write(f"Chatterbot: {welcome(user_input)}")
+    if submit_button and user_input:
+        bow_response = chat_bow(user_input)
+        tfidf_response = chat_tfidf(user_input)
         
-        else:
-            response = generateResponse(user_input)
-            st.write("Chatterbot:", response)
+        st.write("BOW Response:", bow_response)
+        st.write("TF-IDF Response:", tfidf_response)
 
 if __name__ == "__main__":
     main()
